dd35472eb444321fde41442efbbc71640c607c54
Travis CI User <travis@example.org>
2020-04-08 08:34:01 +0000

    Updates

--- src/components/navigation.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export default (props) => (
-  <nav className="navigation"> 
-    <a href="https://t.me/ifpc_channel">Telegram</a>
-    <a href="https://github.com/irfpc">Github</a>
-  </nav>
-  
-)
